e18bf78c8f44ab449a8b9b3089eda3bc31c8c550
Roberto Anic Banic <rxa6313@rit.edu>
2018-06-05 16:12:57 +0200

    Add Crashlytics
    Dependecy added in commit before

--- app/src/main/java/com/aconno/acnsensa/AcnSensaApplication.kt
+++ app/src/main/java/com/aconno/acnsensa/AcnSensaApplication.kt
@@ -4,7 +4,10 @@ import android.app.Application
 import com.aconno.acnsensa.dagger.application.AppComponent
 import com.aconno.acnsensa.dagger.application.AppModule
 import com.aconno.acnsensa.dagger.application.DaggerAppComponent
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
+
 
 /**
  * @author aconno
@@ -13,13 +16,14 @@ class AcnSensaApplication : Application() {
 
     val appComponent: AppComponent by lazy {
         DaggerAppComponent
-            .builder()
-            .appModule(AppModule(this))
-            .build()
+                .builder()
+                .appModule(AppModule(this))
+                .build()
     }
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        Fabric.with(this, Crashlytics())
     }
 }
