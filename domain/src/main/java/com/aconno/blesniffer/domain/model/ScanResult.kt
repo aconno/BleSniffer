2e88e2dab21ead8d70ce1d84a407653f8cacb09a
Alen Magdic <alen.magdic123@gmail.com>
2020-06-23 17:00:10 +0200

    Fixed merge issue

--- domain/src/main/java/com/aconno/blesniffer/domain/model/ScanResult.kt
+++ domain/src/main/java/com/aconno/blesniffer/domain/model/ScanResult.kt
@@ -1,6 +1,7 @@
 package com.aconno.blesniffer.domain.model
 
-data class ScanResult(val device: Device, val advertisement: Advertisement, var timestamp: Long, var rssi: Int) {
+data class ScanResult(val device: Device, val advertisement: Advertisement, var timestamp: Long, var rssi: Int,var timeFromLastTimestamp: Long = -1) {
+
     override fun hashCode(): Int {
         var result = device.macAddress.hashCode()
         result = 31 * result + advertisement.rawData.contentHashCode()
