a88849a160d52b84cdb2a3aff3be4d45f524348c
Alen Magdic <alen.magdic123@gmail.com>
2020-07-16 16:24:40 +0200

    Handled generating sample data when there is a field with end index lower than start index

--- domain/src/main/java/com/aconno/blesniffer/domain/interactor/deserializing/GenerateSampleDataUseCase.kt
+++ domain/src/main/java/com/aconno/blesniffer/domain/interactor/deserializing/GenerateSampleDataUseCase.kt
@@ -4,18 +4,23 @@ import com.aconno.blesniffer.domain.deserializing.Deserializer
 import com.aconno.blesniffer.domain.deserializing.sampledatagenerator.SampleDataGenerator
 import com.aconno.blesniffer.domain.interactor.type.SingleUseCaseWithParameter
 import io.reactivex.Single
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class GenerateSampleDataUseCase : SingleUseCaseWithParameter<ByteArray,Deserializer> {
 
     override fun execute(parameter: Deserializer): Single<ByteArray> {
-        val requiredLength = parameter.fieldDeserializers.map { it.endIndexExclusive }.max()
-            ?: return Single.just(byteArrayOf())
+        val requiredLength = max(
+            parameter.fieldDeserializers.map { it.endIndexExclusive }.max() ?: 0,
+            parameter.fieldDeserializers.map { it.startIndexInclusive }.max() ?: 0
+        )
 
         val bytes = ByteArray(requiredLength)
         bytes.forEachIndexed { index, byte -> bytes[index] = 0xFF.toByte() }
 
         parameter.fieldDeserializers.forEach {
-            SampleDataGenerator.generateSampleValueForType(it.type,it.endIndexExclusive - it.startIndexInclusive).copyInto(bytes,it.startIndexInclusive)
+            SampleDataGenerator.generateSampleValueForType(it.type,abs(it.endIndexExclusive - it.startIndexInclusive)).copyInto(bytes,min(it.startIndexInclusive,it.endIndexExclusive))
         }
 
         return Single.just(bytes)
