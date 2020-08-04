ada8e9b17fe749928fcd0c5d5b57b9985d0789f3
Alen Magdic <alen.magdic123@gmail.com>
2020-08-04 18:54:18 +0200

    Added plain hex formatter

---
+++ hexinputlib/src/main/java/com/aconno/hexinputlib/formatter/PlainHexFormatter.kt
@@ -0,0 +1,28 @@
+package com.aconno.hexinputlib.formatter
+
+import com.aconno.hexinputlib.isHexChar
+
+class PlainHexFormatter : HexFormatter {
+
+    override fun format(values: List<Char>): String {
+        return HexFormattersUtils.hexValuesToValuePairs(values).joinToString { "" }
+    }
+
+    override fun parse(text: String): List<Char> {
+        val trimmedText = text.trim()
+        if(trimmedText.find { !it.isHexChar() } != null) {
+            throw IncompatibleFormatException()
+        }
+        return trimmedText.toCharArray().toList()
+    }
+
+    override fun locateSourceValue(values: List<Char>, formattedValueIndex: Int): Int {
+        return formattedValueIndex
+    }
+
+    override fun locateFormattedValue(values: List<Char>, sourceIndex: Int): Int {
+        return sourceIndex
+    }
+
+
+}
