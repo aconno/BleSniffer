f6b73a971fd8c07c1337eb125b892d7d761587d4
Alen Magdic <alen.magdic123@gmail.com>
2020-09-18 14:05:06 +0200

    Added javadoc for KeyboardManager

--- hexinputlib/src/main/java/com/aconno/hexinputlib/KeyboardManager.kt
+++ hexinputlib/src/main/java/com/aconno/hexinputlib/KeyboardManager.kt
@@ -8,9 +8,28 @@ import android.view.inputmethod.InputMethodManager
 import com.aconno.hexinputlib.ui.keyboard.BaseHexKeyboardView
 import java.lang.IllegalStateException
 
+/**
+ * An object providing methods related to handling a hex keyboard and the system keyboard. To be more
+ * specific, it contains method that handle showing and hiding a hex keyboard and the system keyboard,
+ * with some other helpful methods as well. It is highly recommended to use methods of this manager to
+ * handle displaying hex keyboard instead of doing this manually (for example, by directly changing the
+ * visibility of a [BaseHexKeyboardView]) because doing this manually could lead to some issues resulting
+ * in a poor UX.
+ */
 object KeyboardManager {
     private var keyboardHideEventTime : Long = 0
 
+    /**
+     * Finds a [BaseHexKeyboardView] in the view hierarchy of [viewHierarchyMember]. It goes through
+     * the whole hierarchy to find any [BaseHexKeyboardView], not only through the descendants of the
+     * [viewHierarchyMember]. If there is no any [BaseHexKeyboardView] in the view hierarchy, an
+     * [IllegalStateException] is thrown.
+     *
+     * @param viewHierarchyMember member of a view hierarchy that is expected to contain a [BaseHexKeyboardView]
+     *
+     * @return an instance of [BaseHexKeyboardView] found in the view hierarchy of [viewHierarchyMember]
+     * @throws IllegalStateException if there is no any [BaseHexKeyboardView] in the view hierarchy
+     */
     fun findHexKeyboardView(viewHierarchyMember : View) : BaseHexKeyboardView {
         return findHexKeyboardInViewHierarchy(viewHierarchyMember.rootView) ?:
                     throw IllegalStateException(KEYBOARD_VIEW_MISSING_EXCEPTION_MESSAGE)
@@ -30,6 +49,13 @@ object KeyboardManager {
         return null
     }
 
+    /**
+     * Displays the specified [hexKeyboardView]. If the system keyboard is open when this method
+     * gets called, the method will automatically hide it before displaying the [hexKeyboardView].
+     *
+     * @param hexKeyboardView a hex keyboard that should be displayed
+     *
+     */
     fun showHexKeyboard(hexKeyboardView: BaseHexKeyboardView) {
         hideSystemKeyboard(hexKeyboardView)
 
@@ -45,19 +71,44 @@ object KeyboardManager {
         }
     }
 
+    /**
+     * Displays a [BaseHexKeyboardView] contained in the view hierarchy of [viewRequestingKeyboard].
+     * If the system keyboard is open when this method gets called, the method will automatically
+     * hide it before displaying the [BaseHexKeyboardView].
+     *
+     * @param viewRequestingKeyboard view that needs to use a hex keyboard
+     */
     fun showHexKeyboard(viewRequestingKeyboard : View) {
         showHexKeyboard(findHexKeyboardView(viewRequestingKeyboard))
     }
 
+    /**
+     * Hides the specified [hexKeyboardView].
+     *
+     * @param hexKeyboardView a hex keyboard view that is to be hidden
+     */
     fun hideHexKeyboard(hexKeyboardView: BaseHexKeyboardView) {
         hexKeyboardView.visibility = View.GONE
         keyboardHideEventTime = System.currentTimeMillis()
     }
 
+    /**
+     * Hides a [BaseHexKeyboardView] contained in the view hierarchy of [viewToHideFrom]. If there
+     * is no any [BaseHexKeyboardView] in the view hierarchy, an [IllegalStateException] is thrown.
+     *
+     * @param viewToHideFrom member of view hierarchy containing a [BaseHexKeyboardView] that is to be hidden
+     *
+     * @throws IllegalStateException if there is no any [BaseHexKeyboardView] in the view hierarchy of [viewToHideFrom]
+     */
     fun hideHexKeyboard(viewToHideFrom: View) {
         hideHexKeyboard(findHexKeyboardView(viewToHideFrom))
     }
 
+    /**
+     * Hides the system keyboard from [viewToHideFrom].
+     *
+     * @param viewToHideFrom view to hide the system keyboard from
+     */
     fun hideSystemKeyboard(viewToHideFrom: View) {
         val inputMethodManager = viewToHideFrom.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(viewToHideFrom.windowToken,0)
